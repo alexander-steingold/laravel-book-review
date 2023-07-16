347001b9bc015d9a1acd2962419b8f93111c3787
Alexander Steingold <alex461976@gmail.com>
2023-07-16 18:01:23 +0300

    implement add review model and limit requests

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="mb-4">
         <h1 class="mb-2 text-2xl">{{ $book->title }}</h1>
-
         <div class="book-info">
             <div class="book-author mb-4 text-lg font-semibold">by {{ $book->author }}</div>
             <div class="book-rating flex items-center">
@@ -18,30 +17,35 @@
         </div>
     </div>
 
-    <div>
-        <h2 class="mb-4 text-xl font-semibold">Reviews</h2>
-        <ul>
-            @forelse ($book->reviews as $review)
-                <li class="book-item mb-4">
-                    <div>
-                        <div class="mb-2 flex items-center justify-between">
-                            <div class="font-semibold">
-                                {{--                                {{ $review->rating }}--}}
-                                <x-star-rating :rating="$review->rating"/>
-                            </div>
-                            <div class="book-review-count">
-                                {{ $review->created_at->format('M j, Y') }}</div>
+
+
+    <div class="flex justify-between align-center mb-4">
+        <h2 class="text-xl font-semibold">Reviews</h2>
+        <a href="{{ route('books.reviews.create', $book) }}" class="reset-link">Add Review</a>
+    </div>
+
+    <ul>
+        @forelse ($book->reviews as $review)
+            <li class="book-item mb-4">
+                <div>
+                    <div class="mb-2 flex items-center justify-between">
+                        <div class="font-semibold">
+                            {{--                                {{ $review->rating }}--}}
+                            <x-star-rating :rating="$review->rating"/>
                         </div>
-                        <p class="text-gray-700">{{ $review->review }}</p>
+                        <div class="book-review-count">
+                            {{ $review->created_at->format('M j, Y') }}</div>
                     </div>
-                </li>
-            @empty
-                <li class="mb-4">
-                    <div class="empty-book-item">
-                        <p class="empty-text text-lg font-semibold">No reviews yet</p>
-                    </div>
-                </li>
-            @endforelse
-        </ul>
+                    <p class="text-gray-700">{{ $review->review }}</p>
+                </div>
+            </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text text-lg font-semibold">No reviews yet</p>
+                </div>
+            </li>
+        @endforelse
+    </ul>
     </div>
 @endsection
